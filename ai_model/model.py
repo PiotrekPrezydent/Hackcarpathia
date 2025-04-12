f323b35f445ad61b36a63761b010f6454bc5ed0f
konrad <szyszlakkonrad@gmail.com>
2025-04-12 13:18:01 +0200

    coś zwraca

--- ai_model/model.py
+++ ai_model/model.py
@@ -4,17 +4,11 @@ from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 
 # Simulate data (replace this with real data from your watch)
-data = {
-    'heart_rate': [72, 75, 80, 85, 90, 95, 100, 105, 110, 115],
-    'body_temperature': [36.5, 36.6, 36.7, 36.8, 37.0, 37.2, 37.5, 38.0, 38.5, 39.0],
-    'condition': [0, 0, 0, 0, 0, 1, 1, 1, 1, 1]  # 0=healthy, 1=ill
-}
+data = pd.read_csv("Cardiovascular_Disease_Dataset.csv")
+print(data.head())
 
-df = pd.DataFrame(data)
-print(df)
-
-X = df[['heart_rate', 'body_temperature']]  # Features
-y = df['condition']  # Target
+X = data[['chestpain', 'maxheartrate']]  # Features
+y = data['target']  # Target
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
